38ab1307e22356ffcf72fd9c5d2c494cc81fbb7d
Shivasai <38298958+shivasai780@users.noreply.github.com>
2021-09-21 13:46:12 +0530

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider=require("truffle-hdwallet-provider");
 const Web3=require("web3");
 const {interface,bytecode}=require("./compile");
-const provider=new HDWalletProvider('urge firm village vintage clap farm guilt category bracket chef thank machine',' https://rinkeby.infura.io/v3/51223dfaae564927b29e609d0705fa12'
+const provider=new HDWalletProvider('Keep your phasphrase',' https://rinkeby.infura.io/v3/51223dfaae564927b29e609d0705fa12'
 );
 const web3=new Web3(provider);
 const deploy=async()=>{
